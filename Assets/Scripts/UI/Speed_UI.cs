12b1e06c9962bc69365bf9eedf20522a11d5d15e
YoonMinHyuk <ghini7170@gmail.com>
2024-08-07 10:05:14 +0900

    Update Speed_UI.cs

--- Assets/Scripts/UI/Speed_UI.cs
+++ Assets/Scripts/UI/Speed_UI.cs
@@ -6,18 +6,9 @@ using static UnityEngine.Rendering.VirtualTexturing.Debugging;
 
 public class Speed_UI : MonoBehaviour
 {
-    [Header("�̹���")]
-    public Image[] Handle = new Image[2];
-
-
-    [Header("���� ����")]
-    [SerializeField] bool isFuel = false;
     public bool isSwap = false;
-    public float fuel_Time = 10;
-    float t_fuel = 0;
 
-    public float speed_Time = 3;
-    float t_Speed = 0;
+
     Image speed_Slider;
 
     public float swap_Time = 2;
@@ -33,7 +24,6 @@ public class Speed_UI : MonoBehaviour
 
         if (isSwap)
         {
-            Debug.Log("Swap");
             t_Swap += Time.deltaTime / swap_Time;
             speed_Slider.fillAmount = t_Swap >= 0.5 ? 1 : (t_Swap / 0.5f);
 
